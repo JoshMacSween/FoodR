761e6950e6461a4512903208ea795609d9f52227
Joshua MacSween <joshmacsween@gmail.com>
2020-12-29 14:17:10 -0700

    adding to favourites array

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,24 +1,24 @@
 import React, { useState, useContext } from "react";
-import axios from "axios";
-import { Container, Form, Card, Button, Col, Row } from "react-bootstrap";
+
+import { Container, Form, Button, Col, Row } from "react-bootstrap";
 import { UserContext } from "../contexts/UserProvider";
 import { useHistory } from "react-router";
 
 export default function Register() {
   const {
-    formSubmit,
-    user,
-    handleChangeName,
-    handleChangeEmail,
-    handleChangePassword,
-    handleChangeFavourites,
+    formSubmit
   } = useContext(UserContext);
   const [form, setForm] = useState({});
   const [error, setError] = useState(null);
   const history = useHistory();
+  const [favourites, setFavourites] = useState([{ name: "" }]);
 
   const generalChange = event => {
     setForm({ ...form, [event.target.name]: event.target.value });
+  };
+
+  const addFavourite = () => {
+    setFavourites([...favourites, { name: "" }]);
   };
 
   return (
@@ -28,19 +28,13 @@ export default function Register() {
         onSubmit={async e => {
           e.preventDefault();
           try {
-            const response = await formSubmit(form);
+            const response = await formSubmit({ ...form, favourites });
             console.log(response);
             setError(null);
             history.push("/");
           } catch (error) {
             setError("Something went wrong, please try again");
           }
-          // if (response.status < 400) {
-          //   setError(null);
-          //   history.push("/");
-          // } else {
-          //   setError({ message: response.message });
-          // }
         }}
       >
         <h3>{error}</h3>
@@ -66,14 +60,30 @@ export default function Register() {
         {/* <Form.Label>Password Confirm</Form.Label>
         <Form.Control value={form.passwordConfirm} type="password" /> */}
 
-        {/* <Form.Label>Name a few of your favourite local restaurants</Form.Label>
-        <Form.Control
-          value={form.favourites.name}
-          onChange={generalChange}
-          type="text"
-          as="textarea"
-          name="favourites"
-        /> */}
+        <Form.Label>Name a few of your favourite local restaurants</Form.Label>
+
+          {favourites.map((favourite, i) => {
+            return (
+              <Form.Control
+                key={i}
+                value={favourite.name}
+                onChange={e =>
+                  setFavourites([
+                    ...favourites.map((f, index) =>
+                      index === i ? { name: e.target.value } : f
+                    ),
+                  ])
+                }
+                type="text"
+                name={`favourites-${i}`}
+              />
+            );
+          })}
+
+
+          <Button onClick={addFavourite}>+</Button>
+
+
         <Button type="submit" block>
           Submit
         </Button>
